491b075c4a6b91c44ba878122b52363eacddb6f6
subedisuraj1 <sr.subedi1@gmail.com>
2024-11-18 21:11:45 +0545

    Gradientcontainer color task

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/style.dart';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-    body: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors:[
-          Colors.black,
-          Colors.red
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-        )
+        body: GradientContainer(),
       ),
-      child: Text('Hello World'),
-    )
-  ),),);
+    ),
+  );
+}
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: const [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98)
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(child: StyledText('flutter')),
+    );
+  }
 }
